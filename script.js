6b5f06c4c78a132b321574fe18df104e0a3f2f06
Justin <justin.d.munoz@vanderbilt.edu>
2024-12-12 03:02:15 -0500

    Added submitForm function (does nothing yet)

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Make Geometry Dash levels");
+const submitButton = document.getElementById("submit-button")
+const inputName = document.getElementById("input-name")
+const inputId = document.getElementById("input-id")
+const inputCreator = document.getElementById("input-creator")
+const inputVideo = document.getElementById("input-video")
+
+function submitForm() {
+    const name = inputName.value;
+    const id = inputId.value;
+    const creator = inputCreator.value;
+    const video = inputVideo.value;
+    
+    if (!name || !id || !creator || !video) {
+        alert("Please fill in all fields!");
+        return;
+    }
+
+    console.log(name);
+    console.log(id);
+    console.log(creator);
+    console.log(video);
+}
